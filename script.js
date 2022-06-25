61f0b89817a905e6f62601d889744799af8d8e2f
John Olatubosun <toluolatubosun@gmail.com>
2022-06-25 23:04:46 +0100

    chore: prettier

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-const nameInput = document.getElementById('name')
-const downloadBtn = document.getElementById('download-btn')
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const nameInput = document.getElementById("name");
+const downloadBtn = document.getElementById("download-btn");
 
-const image = new Image()
-image.src = 'certificate.png'
+const image = new Image();
+image.src = "certificate.png";
 image.onload = function () {
-	drawImage()
-}
+    drawImage();
+};
 
-function reszie(canvas, width, height){
-	var c = document.createElement('canvas');
-	c.width = width;
-	c.height = height;
-	c.getContext('2d').drawImage(canvas, 0,0,canvas.width, canvas.height, 0,0,width, height);
-	return c.toDataURL('image/png');
+function reszie(canvas, width, height) {
+    var c = document.createElement("canvas");
+    c.width = width;
+    c.height = height;
+    c.getContext("2d").drawImage(
+        canvas,
+        0,
+        0,
+        canvas.width,
+        canvas.height,
+        0,
+        0,
+        width,
+        height
+    );
+    return c.toDataURL("image/png");
 }
 
 function drawImage() {
-	// ctx.clearRect(0, 0, canvas.width, canvas.height)
-	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
-	ctx.font = '70px Alex Brush'
-	ctx.fillStyle = '#13640a'
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    ctx.font = "70px Alex Brush";
+    ctx.fillStyle = "#13640a";
     var textWidth = ctx.measureText(nameInput.value).width;
-	ctx.fillText(nameInput.value, (canvas.width/2) - (textWidth / 2), 300);
-	ctx.imageSmoothingEnabled = false;
+    ctx.fillText(nameInput.value, canvas.width / 2 - textWidth / 2, 300);
+    ctx.imageSmoothingEnabled = false;
 }
 
-nameInput.addEventListener('input', function () {
-	drawImage()
-})
+nameInput.addEventListener("input", function () {
+    drawImage();
+});
 
-downloadBtn.addEventListener('click', function () {
-	downloadBtn.href = reszie(canvas, 2000, 1440)
-	downloadBtn.download = 'Certificate - ' + nameInput.value
-})
+downloadBtn.addEventListener("click", function () {
+    downloadBtn.href = reszie(canvas, 2000, 1440);
+    downloadBtn.download = "Certificate - " + nameInput.value;
+});
